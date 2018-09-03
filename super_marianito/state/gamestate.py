165b9313688cddff5efedb3e2f83f767e2e58f25
Leo Basov <basov.leo@googlemail.com>
2018-09-03 15:21:02 +0200

    Added movement.

--- super_marianito/state/gamestate.py
+++ super_marianito/state/gamestate.py
@@ -7,7 +7,21 @@ from state.level import Level
 class Move:
 
 	def __init__(self):
-		self.move_speed = [0, 0]
+		self.up = False
+		self.down = False
+		self.left = False
+		self.right = False
+
+	def _move(self):
+		mov = [0, 0]
+
+		if self.right:
+			mov[0] -= 5
+
+		if self.left:
+			mov[0] += 5
+
+		return mov
 
 class GameState:
 	"""docstring for Entitiy"""
@@ -15,7 +29,8 @@ class GameState:
 	def __init__(self):
 		self.entities = {}
 		self.level = Level()
-		self.move = Move()
+		self.mover = Move()
+		self.position = [0, 0]
 
 	def create_level(self, file_name):
 		self.level.graphic =  pygame.image.load(file_name)
@@ -30,9 +45,22 @@ class GameState:
 			if event.type == QUIT:
 				pygame.quit()
 				sys.exit()
+			elif event.type == KEYDOWN:
+				if event.key == K_RIGHT:
+					self.mover.right = True
+				elif event.key == K_LEFT:
+					self.mover.left = True
+			elif event.type == KEYUP:
+				if event.key == K_RIGHT:
+					self.mover.right = False
+				elif event.key == K_LEFT:
+					self.mover.left = False
 
 	def _move(self):
-		pass
+		mov = self.mover._move()
+
+		self.position[0] += mov[0]
+		#self.position[1] += mov[1]
 
 	def _check_collisions(self, events):
 		pass
@@ -50,4 +78,4 @@ class GameState:
 		self._print_level(surface_surf)
 
 	def _print_level(self, surface_surf):
-		surface_surf.blit(self.level.graphic, (0, 0))
+		surface_surf.blit(self.level.graphic, (self.position[0], self.position[1]))
