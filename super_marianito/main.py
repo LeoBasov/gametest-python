9bcf25a5bd76973c323c9a20cf0d7e577b0150d9
Leo Basov <basov.leo@googlemail.com>
2018-09-03 13:17:52 +0200

    Added stuff to main loop.

--- super_marianito/main.py
+++ super_marianito/main.py
@@ -2,7 +2,14 @@ import pygame, sys
 from pygame.locals import *
 
 def main():
-	print(__name__)
+	"""main function of the game
+
+	#read config file and get properties (resolution, ...)
+	#configure game
+
+	#initiize pygame
+
+	#start game loop
 
 if __name__ == '__main__':
 	main()
