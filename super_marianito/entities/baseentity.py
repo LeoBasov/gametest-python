0a813d0a0da4a9f10f8554867bbb99bb5dc5b927
Leo Basov <basov.leo@googlemail.com>
2018-09-03 15:30:02 +0200

    Modified base entity.

--- super_marianito/entities/baseentity.py
+++ super_marianito/entities/baseentity.py
@@ -1,7 +1,17 @@
 """This is the base class for entities in the game"""
 
-class Entitiy(object):
+class Entitiy:
 	"""docstring for Entitiy"""
 
 	def __init__(self):
+		self.position = [0, 0]
+		self.grapic  = []
+
+	def process_events(self, events):
 		pass
+
+	def move(self, addition):
+		pass
+
+	def print(self, surface_surf):
+		surface_surf.blit(self.graphic, (self.position[0], self.position[1]))
