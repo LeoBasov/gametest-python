410207a77b2bb77e813c49514f466df162aefc09
Leo Basov <basov.leo@googlemail.com>
2018-09-03 14:04:46 +0200

    Implemented main.py

--- super_marianito/main.py
+++ super_marianito/main.py
@@ -1,15 +1,29 @@
 import pygame, sys
 from pygame.locals import *
+from config import parser
 
 def main():
-	"""main function of the game
+	"""main function of the game"""
 
-	#read config file and get properties (resolution, ...)
-	#configure game
+	#read config
+	config = parser.get_game_state('./config/config.xml')
 
 	#initiize pygame
+	pygame.init()
+	fps_clock = pygame.time.Clock()
+	DISPLAYSURF = pygame.display.set_mode((config.resolution[0], config.resolution[1]))
+	pygame.display.set_caption('SUPER Marianito')
 
 	#start game loop
+	while True:
+		#handle events
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+
+	pygame.display.update()
+	fps_clock.tick(config.fps)
 
 if __name__ == '__main__':
 	main()
