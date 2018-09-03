8b303b4950977fa504ff226aec197eab3d2849e5
Leo Basov <basov.leo@googlemail.com>
2018-09-03 12:50:48 +0200

    Added game stuff

---
+++ super_marianito/entities/baseentity.py
@@ -0,0 +1,7 @@
+"""This is the base class for entities in the game"""
+
+class Entitiy(object):
+	"""docstring for Entitiy"""
+
+	def __init__(self):
+		pass
---
+++ super_marianito/main.py
@@ -0,0 +1,8 @@
+import pygame, sys
+from pygame.locals import *
+
+def main():
+	print(__name__)
+
+if __name__ == '__main__':
+	main()
