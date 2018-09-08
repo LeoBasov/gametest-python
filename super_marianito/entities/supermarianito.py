042ef6957a80008efe68b99d0a885bf25f5c1027
Leo Basov <basov.leo@googlemail.com>
2018-09-08 18:59:26 +0200

    Implemented jumping.

--- super_marianito/entities/supermarianito.py
+++ super_marianito/entities/supermarianito.py
@@ -29,7 +29,28 @@ class Jumping(BaseState):
 	def __init__(self, position):
 		super().__init__(position)
 
+		self.start = self.position[1]
+		self.height = 50
+		self.jump_iter_step = 0.1
+		self.jump_iter = self.jump_iter_step
+		self.done = False
+
 		self.load_animation_step('./graphics/sup_jump.png', './graphics/sup_jump_back.png')
+
+	def reset(self, front):
+		self.jump_iter = self.jump_iter_step
+		self.start = self.position[1]
+		self.animation_iter = 0
+		self.animation_index = 0
+		self.front = front
+		self.done = False
+
+	def _move(self, addition):
+		self.position[1] = self.start - self.height*math.sin(self.jump_iter)
+		self.jump_iter += self.jump_iter_step
+
+		if self.position[1] <= self.start - 0.99*self.height:
+			self.done = True
 
 class Falling(BaseState):
 	"""docstring"""
@@ -39,13 +60,16 @@ class Falling(BaseState):
 
 		self.load_animation_step('./graphics/sup_jump.png', './graphics/sup_jump_back.png')
 
+	def _move(self, addition):
+		pass
+
 class SuperMarianito(Entitiy):
 	"""docstring for ClassName"""
 
 	def __init__(self):
 		super().__init__()
 
-		self.state_step = 'stand'
+		self.state_step = 'jump'
 
 		self.position[0] = 50
 		self.position[1] = 180
@@ -63,6 +87,10 @@ class SuperMarianito(Entitiy):
 		pass
 
 	def process_events(self, events):
+		if self.state_step=='jump' and self.states[self.state_step].done:
+			self.state_step = 'fall'
+			self.states[self.state_step].reset(self.states[self.state_step].front)
+
 		for event in events:
 			if (event.type == KEYDOWN) and (event.key == K_RIGHT) and ((not self.state_step=='run') or (not self.states[self.state_step].front)):
 				self.state_step = 'run'
